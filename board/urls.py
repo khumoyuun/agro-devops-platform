f62960cb93f366405213a2caff32fae2cbce2522
humoyun <uktamovhumoyun046@gmail.com>
2026-01-18 12:54:59 +0500

    change urls

--- board/urls.py
+++ board/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import product_list, health
+from .views import product_list, health, dashboard
 
 urlpatterns = [
     path("", product_list, name="product_list"),
