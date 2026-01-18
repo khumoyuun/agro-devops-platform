adebc0b9074c0d85bfab483e56f5dfe508d00cf3
humoyun <uktamovhumoyun046@gmail.com>
2026-01-18 12:19:13 +0500

    Add Django admin for Product model

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "quantity", "unit", "price", "updated_at")
+    search_fields = ("name",)
+    list_filter = ("updated_at",)
