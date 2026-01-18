1b3fb6da6498a653b47c155e5ea2d6119da067b3
humoyun <uktamovhumoyun046@gmail.com>
2026-01-18 10:06:02 +0500

    Add basic test for CI

--- board/tests.py
+++ board/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+class ProductModelTest(TestCase):
+    def test_product_str(self):
+        p = Product.objects.create(name="Potato", quantity=1, unit="kg", price="1000.00")
+        self.assertEqual(str(p), "Potato")
