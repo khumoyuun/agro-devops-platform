d337ce55e27f7c9d67c4ddc3c49dc71ef5c346ce
humoyun <uktamovhumoyun046@gmail.com>
2026-01-18 12:48:14 +0500

    change views.py

--- board/views.py
+++ board/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Order
 from django.http import JsonResponse
-
+from django.utils.timezone import now
 
 def product_list(request):
     products = Product.objects.order_by("-updated_at")
@@ -10,3 +10,12 @@ def product_list(request):
 
 def health(request):
     return JsonResponse({"status": "ok"})
+
+def dashboard(request):
+    data = {
+        "products_count": Product.objects.count(),
+        "orders_count": Order.objects.count(),
+        "last_product_update": Product.objects.order_by("-updated_at").values_list("updated_at", flat=True).first(),
+        "server_time": now(),
+    }
+    return render(request, "board/dashboard.html", data)
