82cd77c45e2e0379aca967813c6ad0a667b1a175
humoyun <uktamovhumoyun046@gmail.com>
2026-01-18 12:53:48 +0500

    change htmls

--- board/urls.py
+++ board/urls.py
@@ -4,4 +4,5 @@ from .views import product_list, health
 urlpatterns = [
     path("", product_list, name="product_list"),
     path("health/", health, name="health"),
+    path("dashboard/", dashboard, name="dashboard"),
 ]
