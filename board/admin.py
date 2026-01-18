06c09f3eb29189b12fb21730df2e9ce0c9fa5b2c
humoyun <uktamovhumoyun046@gmail.com>
2026-01-18 14:47:17 +0500

    Log new orders from admin

--- board/admin.py
+++ board/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
-from .models import Product
-from .models import Product, Order
-
+from .models import Product, Order, PriceHistory
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "quantity", "unit", "price", "updated_at")
     search_fields = ("name",)
-    list_filter = ("updated_at",)
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ("id", "product", "customer_name", "customer_email", "quantity", "created_at")
     search_fields = ("customer_name", "customer_email")
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        # 100% Render logda chiqadi (stdout)
+        print(
+            f"NEW ORDER: product={obj.product.name} customer={obj.customer_name} "
+            f"email={obj.customer_email} qty={obj.quantity}"
+        )
+
+@admin.register(PriceHistory)
+class PriceHistoryAdmin(admin.ModelAdmin):
+    list_display = ("product", "old_price", "new_price", "changed_at")
